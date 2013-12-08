933e5d334358acd3de005b6f42a209a680a71afc
MatthewTeta <randommagicmatt@gmail.com>
2013-12-07 18:32:44 -0700

    hfgdhjmsf

--- code.js
+++ code.js
@@ -5,16 +5,16 @@ var qwertyUsed = 0;
 
 function codeTest() {
 	code = document.getElementById('code');
-	if((code.value == "asdf"))
+	if(code.value == "asdf")
 	{
-		if (asdfUsed == 0) {}
+		if (asdfUsed == 0) {
 			pts = pts + 1;
 			alert(pts);
 		} else {
 			alert("You have already used that code!")
 		}
 		asdfUsed = 1;
-	} else if((code.value == "qwerty")) {
+	} else if(code.value == "qwerty") {
 		if (qwertyUsed == 0) {
 			pts++;
 			alert(pts);
